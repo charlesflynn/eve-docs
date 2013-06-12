aeab34d71774500f78b22dd0450b8e22e1603181
Charles Flynn <cflynn@irab.org>
2013-06-12 14:19:33 -0500

    removed rawcfg

--- eve_docs/__init__.py
+++ eve_docs/__init__.py
@@ -1,7 +1,5 @@
 from flask import Blueprint, render_template, current_app, jsonify
 from config import get_cfg
-from wadl import get_wadl
-import pprint
 
 
 eve_docs = Blueprint('eve_docs', __name__,
